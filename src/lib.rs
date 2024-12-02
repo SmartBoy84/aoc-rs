2fe493297ab749a370af1957497566334a65b058
John Smith <www.hamdanmahmood12@gmail.com>
2024-12-02 14:59:13 +0800

    sol

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![feature(iter_array_chunks)] // used in 2020:2
+#![feature(array_windows)] // 2024:2
+#![feature(let_chains)] // 2024:2
 
 
 pub mod solutions;
--- src/solutions/y2024/day2.rs
+++ src/solutions/y2024/day2.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::utils;
 
 pub fn main(input: &str) -> (usize, usize) {
@@ -6,7 +8,25 @@ pub fn main(input: &str) -> (usize, usize) {
         .map(|r| r.split(' ').map(utils::parse_int).collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-let part1 = input.iter().filter(|l| l.array_windows::<2>())
+    let part1 = input.iter().filter(|l| {
+        let mut ord = None;
+        for &[a, b] in l.array_windows::<2>() {
+            let ord_n = a.cmp(&b);
+            if ord_n != Ordering::Equal {
+                if let Some(o) = ord {
+                    if ord_n != o {
+                        return false;
+                    }
+                } else {
+                    ord = Some(ord_n)
+                }
+            }
+            if !(1..=3).contains(&a.abs_diff(b)) {
+                return false;
+            }
+        }
+        return true;
+    }).count();
 
-    todo!()
+    (part1, 1)
 }
